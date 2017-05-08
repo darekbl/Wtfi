407009e9fa09a70a43a857cba5ebd658bce4ff6a
darekbl <daro477@interia.pl>
2017-05-08 20:09:04 +0200

    Browsing files and look on the content.

---
+++ Wtfi/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+
+namespace Wtfi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        OpenFileDialog dialog = new OpenFileDialog();
+        string safeXmlFileName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            // Show open file dialog box
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(dialog.FileName);
+                //fileName.Text = dialog.FileName;
+                safeXmlFileName = dialog.SafeFileName;
+                fileName.Text = safeXmlFileName;
+                Console.WriteLine("Filename:{0}", safeXmlFileName);
+                sr.Close();
+            }
+        }
+
+        private void doitButton_Click(object sender, RoutedEventArgs e)
+        {
+            testClass.Test(keyBox, valueBox, safeXmlFileName);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AboutBox about = new AboutBox();
+            about.Show();
+        }
+    }
+}
---
+++ Wtfi/testClass.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
+
+namespace Wtfi
+{
+    class testClass
+    {
+        public string testString { get; set; }
+        public int testInt { get; set; }
+        
+        TextBox keyBox = new TextBox();
+
+        public testClass(string testString="", int testInt=1)
+        {
+            this.testString = testString;
+            this.testInt = testInt;
+        }
+
+        public static void Test(TextBox keyBox, TextBox valueBox, string fileName)
+        {
+            XmlTextReader reader;
+
+            try
+            {
+                reader = new XmlTextReader(fileName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                reader = null;
+                MessageBox.Show(e.Message);
+            }
+            
+            if(reader != null)
+            while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            keyBox.AppendText(reader.Name + "\n");
+                            Console.WriteLine("Element");
+                            break;
+                        case XmlNodeType.Text: //Display the text in each element.
+                            valueBox.AppendText(reader.Value + "\n");
+                            Console.WriteLine("Text");
+                            break;
+                        case XmlNodeType.EndElement: //Display the end of the element.
+                            //keyBox.AppendText(reader.Name + "\n");
+                            Console.WriteLine("EndElement");
+                            break;
+                    }
+                }
+            return;
+        }
+    }
+}
